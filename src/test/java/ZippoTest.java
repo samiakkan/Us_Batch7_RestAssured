d42b22d18fd70af12d79680ddee27b74ea0f0153
Sam <akkansami9@gmail.com>
2023-04-17 22:05:44 -0400

    1st update

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,17 +1,13 @@
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
-
-// pm                                               // Rest Assured
-// pm.response.json().'post code'                   // body("'post code'", ...)
-// pm.response.json().places[0].'place name'        // body("places[0].'place name'", ...)
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
+
     @Test
-    public void test() {
+    public void test(){
 
         given()  // preparation (token, request body, parameters, cookies...)
 
@@ -22,7 +18,7 @@ public class ZippoTest {
     }
 
     @Test
-    public void statusCodeTest() {
+    public void statusCodeTest(){
 
         given()
 
@@ -35,7 +31,7 @@ public class ZippoTest {
     }
 
     @Test
-    public void contentTypeTest() {
+    public void contentTypeTest(){
 
         given()
 
@@ -49,21 +45,80 @@ public class ZippoTest {
     }
 
     @Test
-    public void checkCountryFromResponseBody() {
+    public void checkCountryFromResponseBody(){
         given()
                 .when()
                 .get("http://api.zippopotam.us/us/90210")
                 .then()
                 .log().body()
-                .body("country", equalTo("United States"));
-
+                .body("country",equalTo("United States"));
     }
+
+    // pm                                               // Rest Assured
+    // pm.response.json().'post code'                   // body("'post code'", ...)
+    // pm.response.json().places[0].'place name'        // body("places[0].'place name'", ...)
+    // body("places.'place name'") gives a list of place names in the places list
     @Test
     public void checkStateFromResponse(){
         given()
 
                 .when()
-                .get("http://api.zippopotam.us/tr/")
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("places[0].state",equalTo("California")); // checks if the state is California
+    }
+
+    @Test
+    public void bodyArrayHasItem(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/tr/01000")
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places.'place name'",hasItem("Büyükdikili Köyü")); // checks if the list of place names has this value
+    }
+
+    @Test
+    public void bodyArraySizeTest(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places",hasSize(1));
+    }
+
+    @Test
+    public void bodyArraySizeTest2(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/tr/01000")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("places.'place name'",hasSize(71)); // checks if the size of the list of place names is 71
+    }
+
+    @Test
+    public void multipleTests(){
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/tr/01000")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("places",hasSize(71))
+                .body("places.'place name'",hasItem("Büyükdikili Köyü"))
+                .body("places[2].'place name'",equalTo("Dörtağaç Köyü"));
     }
 
 }
